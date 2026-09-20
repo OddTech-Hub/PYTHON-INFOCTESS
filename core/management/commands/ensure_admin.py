b5b5c130643b79bde36e561af63edec5994dea78
OddTech-Hub <odemmzy@gmail.com>
2026-09-20 16:30:14 +0000

    Fix ensure_admin user creation logic

--- core/management/commands/ensure_admin.py
+++ core/management/commands/ensure_admin.py
@@ -10,18 +10,25 @@ class Command(BaseCommand):
         
         user = User.objects.filter(email=email).first() or User.objects.filter(username=email).first()
         if not user:
-            user = User.objects.create_superuser(
+            user = User(
                 username=email,
                 email=email,
-                password=password,
                 role=User.Role.LECTURER,
                 first_name='Admin',
-                last_name='User'
+                last_name='User',
+                is_superuser=True,
+                is_staff=True,
+                is_active=True,
+                must_change_password=False
             )
+            user.set_password(password)
+            user.save()
             self.stdout.write(self.style.SUCCESS(f'Successfully created superuser {email}'))
         else:
             user.set_password(password)
             user.is_superuser = True
             user.is_staff = True
+            user.is_active = True
+            user.must_change_password = False
             user.save()
             self.stdout.write(self.style.SUCCESS(f'Successfully updated superuser {email} password'))
